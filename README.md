e936f6b9f461c4443a9f23a2f444031dad230195
Adam Parrish <aparrish@neosavvy.com>
2018-03-19 15:00:38 -0400

    Feature initial layout (#1)
    * Setting up some initial layout for an electron app to submit orders.
    
    * Support for hot keys.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# fast-order
-Fast Order Entry for Crypto Currencies
+# Fast Order Tools (MVP)
+  
+  As a trader in the crypto-currency market you need to be able to take advantage of rapid price movement by submitting trades without the use of your keyboard. Successful equity market traders have numerous hot keys mapped to order modification commands. 
+  
+  For the immediate term this tool will only be for order entry and will not enable Positions Management, Order Management, Trade History and Account Balances. Those features will be exposed through current features of the exchanges the trader is issuing trades on.
+  
+  We need to be able to run the application locally on a desktop not via web interface as speed and responsiveness of the interface will be incredibly critical for this application. 
+  
+  The following commands will be required in the onset of this application
+   * Ctrl O - should open a new order entry window
+   * Ctrl Tab - should switch focus between OE windows
+   * Ctrl S or Ctrl B - should set the ticket as a Sell or Buy ticket
+   * Ctrl E - should iterate through exchanges on focused OE Window
+   * Ctrl P <currencyPair> - should auto filter and help set the currency pair for an exchange
+   * Ctrl j and Ctrl k - increase / decrease quantity by 1%
+   * Ctrl l and Ctrl ; - increase / decrease quantity by 10%
+   * Ctrl t - iterates through Market, Limit, and Stop Loss Orders
+   * Ctrl X - Executes / Sends to the exchange 
+  
+  Orders should be submitted to a FIX Order Handler internally on our client application. Order Management for user side orders should also be handled via FIX (cancel orders for example, and display of order status/fills)
+  
+  Price Action, Portfolio Balance Display across exchanges, and generally low priority data should be managed via a WebSocket. 
+  
+  We should generally avoid calling the REST API in this application, as it can at times result in rate limiting our client by IP address, which would result in incredibly unreliable order execution. 
+  
+  
+  We will create a single order handler that adapts our client side order object into each exchange’s order object and submission protocol may it be FIX, WebSocket or REST. We will favor the protocols in the order of FIX, WebSocket then REST to ensure highest reliability and fastest execution. 
+
+
+### HELPFUL DEV NOTES
+
+- Kill all open electron windows 
+```
+kill -9 `ps aux | awk '/[e]lectron/{print $2}'`
+```
--- main.js
+++ main.js
@@ -1,17 +1,87 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
+const {
+    app,
+    BrowserWindow,
+    BrowserView,
+    globalShortcut
+} = require('electron');
 
-function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+const path = require('path');
+const url = require('url');
+const _ = require('lodash');
 
-    // and load the index.html of the app.
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
+let _win, _view, _controls, _orderWindows = [];
+
+function initializeMainWindow() {
+    let win = new BrowserWindow({
+        closable: false,
+        width: 1024,
+        height: 768});
+    view = new BrowserView({
+        webPreferences: {
+            nodeIntegration: false
+        }
+    });
+    win.setBrowserView(view);
+    view.setBounds({
+        x: 0,
+        y: 0,
+        width: 1024,
+        height: 768
+    });
+    view.webContents.loadURL('https://www.tradingview.com/chart/ntf03khN/');
+
+    return win;
 }
 
-app.on('ready', createWindow)
+function initializeControls(x, y) {
+    let controls = new BrowserWindow({
+        x: x,
+        y: y-60,
+        width: 1024,
+        height: 60});
+
+    controls.loadURL(url.format({
+        pathname: path.join(__dirname, 'controls.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    controls.on('closed', () => {
+        process.exit();
+    });
+
+    return controls;
+}
+
+function newOrderWindow(x, y) {
+    let orderWindow = new BrowserWindow({
+        x: x,
+        y: y,
+        closable: true,
+        width: 400,
+        height: 420});
+    orderWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'order_window.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    return orderWindow;
+}
+
+function initializeApplication () {
+    // Create the browser window.
+    _win = initializeMainWindow();
+    const chartBounds = _win.getBounds();
+    _controls = initializeControls(chartBounds.x, chartBounds.y);
+    _orderWindows.push(newOrderWindow(chartBounds.x, chartBounds.y + chartBounds.height));
+}
+
+app.on('ready', () => {
+    initializeApplication();
+    globalShortcut.register('CommandOrControl+N', () => {
+        const orderWindowBounds = _.last(_orderWindows).getBounds();
+        _orderWindows.push(newOrderWindow(orderWindowBounds.x + 400, orderWindowBounds.y));
+    })
+
+});
